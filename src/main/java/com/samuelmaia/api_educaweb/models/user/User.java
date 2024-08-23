ed6e98c99a7ea0edeef23e37e2ad1e377b04416a
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2024-08-23 20:06:20 -0300

    Criando classe User

--- src/main/java/com/samuelmaia/api_educaweb/models/user/User.java
+++ src/main/java/com/samuelmaia/api_educaweb/models/user/User.java
@@ -1,4 +1,26 @@
 package com.samuelmaia.api_educaweb.models.user;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.Id;
+
+@Table(name = "users")
+@Entity(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
 public class User {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    @Column(unique = true)
+    private String login;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
 }
--- src/main/java/com/samuelmaia/api_educaweb/models/user/UserRepository.java
+++ src/main/java/com/samuelmaia/api_educaweb/models/user/UserRepository.java
@@ -2,6 +2,8 @@ package com.samuelmaia.api_educaweb.models.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
-    
+    Optional<User> findByLogin(String login);
 }
