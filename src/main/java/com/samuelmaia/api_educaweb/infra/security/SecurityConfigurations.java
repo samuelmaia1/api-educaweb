7b1d758752c181d85cf3a7b7e2784ec521a531f1
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2024-09-15 01:34:03 -0300

    Resolvendo problema de cors

--- src/main/java/com/samuelmaia/api_educaweb/infra/security/SecurityConfigurations.java
+++ src/main/java/com/samuelmaia/api_educaweb/infra/security/SecurityConfigurations.java
@@ -16,6 +16,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
@@ -27,11 +29,11 @@ public class SecurityConfigurations {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
+                .cors(withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/api/student/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/student/register").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/swagger-ui/index.html#").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
